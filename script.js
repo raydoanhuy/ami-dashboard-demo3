e7e2ea2d1f1e46cf47b43bf810cdc98ca567c10f
raydoanhuy <raydoanhuy@gmail.com>
2025-04-13 22:47:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function loadProjects() {
+  const tbody = document.getElementById("data-body");
+  projects.forEach(p => {
+    const row = document.createElement("tr");
+    row.className = `risk-${p.risk}`;
+
+    row.innerHTML = `
+      <td>${p.name}</td>
+      <td>${p.cod}</td>
+      <td>${p.acceptance}</td>
+      <td>${p.fit}</td>
+      <td>${p.risk.replace("_", " ").toUpperCase()}</td>
+      <td>${p.recommendation}</td>
+    `;
+    tbody.appendChild(row);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", loadProjects);
 
